f483a5687f4699129e2f9008e7bca760b510aedc
Jonathan Hunter <jh-git@ninja.org.uk>
2024-10-15 15:13:07 +0100

    Update Keyestudio ballet to speed 150 (Also parameterise the speed on this sketch, so we can more easily adjust if needed)

--- KeyeStudio/Ballet/Ballet.ino
+++ KeyeStudio/Ballet/Ballet.ino
@@ -5,51 +5,53 @@
 // You don't need to change anything in this code sketch.
 // Just upload it straight to the robot.
 
+const int WHEELSPEED = 150;
+
 void loop()
 {
-	forward(1000,255,255);  // forward basic movement routine for one second at full speed
+	forward(1000,WHEELSPEED,WHEELSPEED);  // forward basic movement routine for one second at full speed
 	halt(500);
 
-	reverse(1000,255,255);  // reverse basic movement routine for one second at full speed
+	reverse(1000,WHEELSPEED,WHEELSPEED);  // reverse basic movement routine for one second at full speed
 	halt(500);
 
-	leftSpin(2000,255);     // turning left basic movement routine for two seconds at full speed
+	leftSpin(2000,WHEELSPEED);     // turning left basic movement routine for two seconds at full speed
 	halt(500);
 
-	rightSpin(2000,255);    // turning right basic movement routine for two seconds at full speed
+	rightSpin(2000,WHEELSPEED);    // turning right basic movement routine for two seconds at full speed
 	halt(500);
 
-	forward(1000,255,255);  // forward basic movement routine for one second at full speed
+	forward(1000,WHEELSPEED,WHEELSPEED);  // forward basic movement routine for one second at full speed
 	halt(500);
 
-	leftSpin(800,255);      // turning left basic movement routine for 4/5 of a second at full speed
+	leftSpin(800,WHEELSPEED);      // turning left basic movement routine for 4/5 of a second at full speed
 	halt(500);
 
-	forward(500,255,255);   // forward basic movement routine for 1/2 a second at full speed
+	forward(500,WHEELSPEED,WHEELSPEED);   // forward basic movement routine for 1/2 a second at full speed
 	halt(500);
 
-	reverse(1000,255,255);  // reverse basic movement routine for one second at full speed
+	reverse(1000,WHEELSPEED,WHEELSPEED);  // reverse basic movement routine for one second at full speed
 	halt(500);
 
 // SHOOGLE!!!
 
-	rightSpin(800,255);     // turning right basic movement routine for 4/5 of a second at full speed
+	rightSpin(800,WHEELSPEED);     // turning right basic movement routine for 4/5 of a second at full speed
 	halt(500);              // wait for 1/2 a second
 	shoogle();              // run the shoogle sub-routine
 
-	rightSpin(800,255);     // turning right basic movement routine for 4/5 of a second at full speed
+	rightSpin(800,WHEELSPEED);     // turning right basic movement routine for 4/5 of a second at full speed
 	halt(500);              // wait for 1/2 a second
 	shoogle();              // run the shoogle sub-routine
 
-	leftSpin(500,255);      // turning left basic movement routine for 1/2 a second at full speed
+	leftSpin(500,WHEELSPEED);      // turning left basic movement routine for 1/2 a second at full speed
 	halt(500);              // wait for 1/2 a second
 	shoogle();              // run the shoogle sub-routine
 
-	leftSpin(500,255);      // turning left basic movement routine for 1/2 a second at full speed
+	leftSpin(500,WHEELSPEED);      // turning left basic movement routine for 1/2 a second at full speed
 	halt(500);              // wait for 1/2 a second
 	shoogle();              // run the shoogle sub-routine
 
-	leftSpin(3000,255);     // turning left basic movement routine for 3 seconds at full speed
+	leftSpin(3000,WHEELSPEED);     // turning left basic movement routine for 3 seconds at full speed
 	halt(500);              // wait for 1/2 a second
 
 	return;
@@ -62,10 +64,10 @@ void shoogle()
 	i = 5;
 	while (i>0)
 	{
-		forward(100,255,255);  // forward basic movement routine for 1/10 of a second at full speed
+		forward(100,WHEELSPEED,WHEELSPEED);  // forward basic movement routine for 1/10 of a second at full speed
 		halt(200);             // wait for 1/5 of a second
 
-		reverse(100,255,255);  // reverse basic movement routine for 1/10 of a second at full speed
+		reverse(100,WHEELSPEED,WHEELSPEED);  // reverse basic movement routine for 1/10 of a second at full speed
 		halt(200);             // wait for 1/5 of a second
 
 		i--;
