db6cd5698e1c9514a201e334bf960958a302e8d8
jmhunter <19241458+jmhunter@users.noreply.github.com>
2026-05-14 13:58:45 +0100

    Clean up line follow example code

--- LineFollow/LineFollow.ino
+++ LineFollow/LineFollow.ino
@@ -2,32 +2,19 @@
 
 // This is the code sketch to make the Barclays robot follow a BLACK line.
 //                                     
-// Use the sensors underneath the robot to decide what to do in each of the FOUR possible scenarios.
-// Change the code below to make the robot move how you want it to.
+// Use the sensors underneath the robot to decide what to do in each of the
+// FOUR possible scenarios. Change the code below to make the robot move how
+// you want it to.
 
 void loop()
 {
 
+  // You will end up with four blocks of code similar to this one:
   if (leftLineSensor() == BLACK && rightLineSensor() == BLACK )
   {
     //What command should you type in here?
-    forward(10,255,255);
     return;
   }
-
-if (leftLineSensor() == WHITE && rightLineSensor() == BLACK )
-  {
-    rightSpin(10,255);
-    return;
-  }
-
-if (leftLineSensor() == BLACK && rightLineSensor() == WHITE )
-  {
-    leftSpin(10,255);
-    return;
-  }
-
-
 
 
 }
